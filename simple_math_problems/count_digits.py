c6d5099d457c4bb4de3b435f10b02d86b3b60ed9
Kailas PS <kailaspsudheer@gmail.com>
2026-07-29 07:39:12 +0530

    count number of digits problem

---
+++ simple_math_problems/count_digits.py
@@ -0,0 +1,24 @@
+class Solution:
+    # function to count digits.
+    def digit_counter(self, N: int) -> int:
+        N = abs(N)
+        if N == 0:
+            return 1
+        
+        counter = 0
+        while N > 0:
+            # floor divison.
+            N = N // 10
+            counter +=1
+        # returns the counter.
+        return counter
+
+    # alternative solution.
+    def alternative(self, N:int) -> int:
+        N = abs(N)
+        return 0 if N == 0 else len(str(N))
+
+sol = Solution()
+N = -6778
+res = sol.alternative(N)
+print(f"Number of digits in {N} is {res}")  
