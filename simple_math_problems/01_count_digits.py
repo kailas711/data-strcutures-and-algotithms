7c29d526184eddb4c5d8ba89e3a30866cdbebf68
Kailas PS <kailaspsudheer@gmail.com>
2026-07-29 07:51:34 +0530

    added log based solution

--- simple_math_problems/01_count_digits.py
+++ simple_math_problems/01_count_digits.py
@@ -1,3 +1,5 @@
+import math 
+
 class Solution:
     # function to count digits.
     def digit_counter(self, N: int) -> int:
@@ -13,13 +15,21 @@ class Solution:
         # returns the counter.
         return counter
 
-    # alternative solution.
-    def alternative(self, N:int) -> int:
+    # 1st alternative solution.
+    # Convert to string and get the length
+    def alternative_1(self, N:int) -> int:
         N = abs(N)
         return 0 if N == 0 else len(str(N))
+
+    # The logarithmic base 10 of a positive integers gives the number of digits in n
+    # 2nd alternative solution which is optimal 
+    def alternative_2(self, N : int) -> int:
+        # We add 1 to the result to ensure that the count is correct even for numbers that are powers of 10.
+        count = int(math.log10(N)+1)
+        return count
 
 if __name__ == "__main__":
     sol = Solution()
     N = -6778
-    res = sol.alternative(N)
+    res = sol.alternative_2(N)
     print(f"Number of digits in {N} is {res}")  
